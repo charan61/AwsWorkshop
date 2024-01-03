35b6db939eb5e54263089fa8ca729c407636d086
charanm-m <charan.m-m@nagarro.com>
2024-01-03 12:44:13 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const mysql = require('mysql2/promise');
+
+const app = express();
+const port = 3000;
+
+// MySQL database configuration
+const dbConfig = {
+  host: 'database-1.c3k20uw0mfcf.us-east-1.rds.amazonaws.com', // Replace with your RDS endpoint
+  user: 'admin',
+  password: 'charanmm',
+  database: 'database-1',
+};
+
+// Route to fetch data from the database
+app.get('/data', async (req, res) => {
+  try {
+    const connection = await mysql.createConnection(dbConfig);
+    const [rows] = await connection.execute('SELECT * FROM your_table');
+    connection.end();
+    res.json(rows);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
